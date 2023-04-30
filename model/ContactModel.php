4f3664795280527f5a0a862709e37c590147397d
Zach Paul <zachpaul319@gmail.com>
2023-04-30 16:27:33 -0400

    successful getContacts and modified statements

--- model/ContactModel.php
+++ model/ContactModel.php
@@ -3,28 +3,16 @@ require_once(__DIR__ . "/Database.php");
 
 class ContactModel {
     public static function getContacts(int $userId) {
-        $sql = "(SELECT u.userId, u.username, m.text
-FROM tblUsersPLAYPAL u
-JOIN (
-  SELECT 
-    CASE
-      WHEN senderId = '$userId' THEN recipientId
-      ELSE senderId
-    END AS contactId,
-    MAX(timestamp) AS lastMessageTime
-  FROM tblMessagesPLAYPAL
-  WHERE senderId = '$userId' OR recipientId = '$userId'
-  GROUP BY CASE
-      WHEN senderId = '$userId' THEN recipientId
-      ELSE senderId
-    END
-) lastMessages ON u.userId = lastMessages.contactId
-JOIN tblMessagesPLAYPAL m ON (
-  m.senderId = '$userId' AND m.recipientId = lastMessages.contactId 
-  OR m.senderId = lastMessages.contactId AND m.recipientId = '$userId'
-) AND m.timestamp = lastMessages.lastMessageTime)";
-        $results = Database::executeSql($sql);
-        return $results;
+        $sql = "(SELECT u.userId, u.username, m.text FROM tblUsersPLAYPAL u JOIN (SELECT CASE WHEN senderId = ? THEN recipientId ELSE senderId END AS contactId, 
+        MAX(timestamp) AS lastMessageTime FROM tblMessagesPLAYPAL WHERE senderId = ? OR recipientId = ? GROUP BY CASE WHEN senderId = ? THEN recipientId ELSE senderId END, 
+        contactId) lastMessages ON u.userId = lastMessages.contactId JOIN tblMessagesPLAYPAL m ON (m.senderId = ? AND m.recipientId = lastMessages.contactId OR 
+        m.senderId = lastMessages.contactId AND m.recipientId = ?) AND m.timestamp= lastMessages.lastMessageTime WHERE u.userId != ? ORDER BY m.timestamp DESC)";
+        $results = Database::executeSql($sql, "iiiiiii", array($userId, $userId, $userId, $userId, $userId, $userId, $userId));
+        if (! isset(Database::$lastError)) {
+            return $results;
+        } else {
+            return Database::$lastError;
+        }
     }
 
     public static function deleteContact(int $messenger_1_Id, int $messenger_2_Id): bool {
